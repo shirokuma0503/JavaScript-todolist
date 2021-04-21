3b750d7ad8dc7e963717f85e40dc8a55733e2b2d
onimuraaika <aika.onimura@gmail.com>
2021-04-21 10:40:16 +0900

    右クリックでリストを削除・左クリックで打ち消し線追加

--- index.js
+++ index.js
@@ -20,13 +20,31 @@ function add(todo) {
 
   //保存されている情報があれば入力された値(input.value)じゃなくてtodo(リスト)表示する
   if (todo) {
-    todoText = todo;
+    todoText = todo.text;
   }
 
   if (todoText.length > 0) {
     const li = document.createElement("li");
     li.innerText = todoText;
     li.classList.add("list-group-item"); //作ったliにクラスを追加
+
+    //更新時も表示。liにtodoの情報が存在していて、打ち消し線があったとき
+    if (todo && todo.completed) {
+      li.classList.add("text-decoration-line-through");
+    }
+
+    //右クリックで削除する
+    li.addEventListener("contextmenu", function (event) {
+      event.preventDefault();
+      li.remove();
+      saveData();
+    });
+    //左クリックで打ち消し線のクラスを追加・削除
+    li.addEventListener("click", function (event) {
+      li.classList.toggle("text-decoration-line-through"); //toggleはあれば消す・なければ付ける
+      saveData();
+    });
+
     ul.appendChild(li); //ulのchildとしてliを追加
     input.value = ""; //打った文字を空にする
     saveData();
@@ -37,7 +55,11 @@ function saveData() {
   const lists = document.querySelectorAll("li"); //全liタグ取得
   let todos = []; //todos配列を作成
   lists.forEach(list => { //リスト個々を変数listにいれる
-    todos.push(list.innerText); //pushでtodos配列にリストをいれる
+    let todo = {
+      text: list.innerText,
+      completed: list.classList.contains("text-decoration-line-through")
+    };
+    todos.push(todo);
   });
 
   localStorage.setItem("todos", JSON.stringify(todos)); //保存
